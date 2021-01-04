aa6162ac12fe20fe9e774889e0c802c94c982bd0
jamiekolesnik <jamie.kolesnik@gmx.de>
2021-01-04 22:24:58 +0100

    Add Day 09, part 2

--- src/days/Day09.java
+++ src/days/Day09.java
@@ -4,6 +4,7 @@ import resources.AdventDataReader;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,12 +12,45 @@ public class Day09 {
 
     public static void main(String[] args) throws IOException {
         String inputData = AdventDataReader.readFromInputStream("Day09_02.txt");
-        List<Long> vals = formatInputData(inputData);
 
-        System.out.println(getNonFittingValue(vals, 25));
+        List<Long> vals = formatInputData(inputData);
+        int limit = 25;
+        long corruptedVal = getCorruptedValue(vals, limit);
+        long minMaxSumCorruptedWeakness = getMinMaxSumOfCorruptedWeakness(vals, corruptedVal);
+
+        System.out.println(minMaxSumCorruptedWeakness);
     }
 
-    private static long getNonFittingValue(List<Long> vals, int limit) {
+    private static long getMinMaxSumOfCorruptedWeakness(List<Long> vals, long corruptedVal) {
+        List<Long> corruptedWeakness = getCorruptedWeakness(vals, corruptedVal);
+        return Collections.max(corruptedWeakness) + Collections.min(corruptedWeakness);
+    }
+
+    private static List<Long> getCorruptedWeakness(List<Long> vals, long corruptedVal) {
+        boolean weaknessFound = false;
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < vals.size(); i++) {
+            long sum = vals.get(i);
+            for (int j = i + 1; j < vals.size() || sum < corruptedVal; j++) {
+                // accumulate
+                sum += vals.get(j);
+
+                if (sum == corruptedVal) {
+                    start = i;
+                    end = j;
+                    weaknessFound = true;
+                    break;
+                }
+            }
+            if (weaknessFound) break;
+        }
+
+        return vals.subList(start, end);
+    }
+
+    private static long getCorruptedValue(List<Long> vals, int limit) {
         boolean match;
 
         for (int i = limit; i < vals.size(); i++) {
