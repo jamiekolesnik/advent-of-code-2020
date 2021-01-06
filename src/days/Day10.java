bce52d2cf7c6aa680fcfdd9cd21d232d6788eec9
jamiekolesnik <jamie.kolesnik@gmx.de>
2021-01-06 14:36:54 +0100

    Refactor

--- src/days/Day10.java
+++ src/days/Day10.java
@@ -24,13 +24,12 @@ public class Day10 {
         int sum1 = 0;
         int sum3 = 0;
 
-        // Treat the charging outlet near your seat as having an effective joltage rating of 0.
-        int currentJolt = 0;
-
         Collections.sort(jolts);
 
-        for (Integer jolt : jolts) {
-            int diff = jolt - currentJolt;
+        int currentJolt = jolts.get(0);
+
+        for (int i = 1; i < jolts.size(); i++) {
+            int diff = jolts.get(i) - currentJolt;
             currentJolt = diff + currentJolt;
             if (diff == 3) sum3++;
             else sum1++;
